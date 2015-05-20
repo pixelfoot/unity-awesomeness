9fc6a21d1c3d7bebbe13065ea84d4076947569af
Pat Flannery <pflannery111@gmail.com>
2015-05-20 14:06:17 -0700

    Added Script

---
+++ AI/Scripts/simplefollow.js
@@ -0,0 +1,30 @@
+﻿ var Player : Transform;
+ var MoveSpeed = 4;
+ var MaxDist = 10;
+ var MinDist = 5;
+ 
+ 
+ 
+ 
+ function Start () 
+ {
+ 
+ }
+ 
+ function Update () 
+ {
+     transform.LookAt(Player);
+     
+     if(Vector3.Distance(transform.position,Player.position) >= MinDist){
+     
+          transform.position += transform.forward*MoveSpeed*Time.deltaTime;
+ 
+           
+           
+          if(Vector3.Distance(transform.position,Player.position) <= MaxDist)
+              {
+                 //Here Call any function U want Like Shoot at here or something
+    } 
+    
+    }
+ }
