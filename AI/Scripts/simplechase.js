bd6f5e52ea2cbb496ad708cd2d32c299ba293c0d
Pat Flannery <pflannery111@gmail.com>
2016-01-12 16:47:25 -0800

    Small Fix to Simplechase.js
    Removed Editor option.

--- AI/Scripts/simplechase.js
+++ AI/Scripts/simplechase.js
@@ -1,22 +1,18 @@
 @script RequireComponent(CharacterController)
 
-enum hassprite {Yes, No}
 var speed: float = 6.0;
 var gravity: float = 20.0;
 var movespeed: Vector3;
 var target : GameObject;
-var FlipSprite : hassprite;
 
 private var sprite : GameObject;
-
-
 
 
 function Start(){
 
 	speed = Random.Range(4.0, 7.6);
 
-	sprite = gameObject.transform.Find("Sprite").gameObject;
+	sprite = gameObject.transform.Find("sprite").gameObject;
 
 }
 
@@ -45,7 +41,7 @@ function Update() {
     
 
   	//Flip the sprite if you have the option selected in editor.
-  	if (FlipSprite == hassprite.Yes){
+  	if (sprite != null) {
 	
 		if (movespeed.x < 0.0){
 	
